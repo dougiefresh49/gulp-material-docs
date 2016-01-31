7bd1710b5f56e64ea3b53b8ce98a324bb246ff3e
Lasater, Doug <Doug.Lasater@fmr.com>
2016-01-30 19:16:17 -0600

    fix(make docs): fix issue of local path vs relative path to gulp-material-docs src
    Previously, error would occur as it was looking for ./dist/index.html from the directory of the
    running gulpfile.  This allows the user to specify where their node modules are, so finding dist
    folder is easy
    
    closes issue #1

--- index.js
+++ index.js
@@ -12,16 +12,17 @@ module.exports = {
 function makeDocs(opts) {
 
     if(opts) {
-        opts.scripts = helpers.getScripts(opts.materialV).concat(opts.scripts || []);
-        opts.styles = helpers.getStyles(opts.materialV, opts.materailColors).concat(opts.styles || []);
+        opts.nodeModulesDir = opts.nodeModulesDir || 'node_modules/gulp-material-docs/';
+        opts.scripts = helpers.getScripts(opts.nodeModulesDir).concat(opts.scripts || []);
+        opts.styles = helpers.getStyles(opts.nodeModulesDir).concat(opts.styles || []);
         opts.legal = helpers.getLegalObj(opts.legal);
+        opts.template = opts.template || opts.nodeModulesDir + config.folders.dist + 'index.html';
+        opts.image = opts.image || opts.nodeModulesDir + config.folders.dist + 'material-docs.svg';
     }
 
     else {
         opts = helpers.getDefaultOptions();
     }
-    opts.image = opts.image || config.folders.dist + 'material-docs.svg';
-    opts.template = opts.template || config.folders.dist + 'index.html';
 
     return ngDocs.process(opts);
 }
